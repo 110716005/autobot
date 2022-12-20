6560fa105495c18cddb18f50fb553503664c6fb4
IHsunChen <s0935959625@gmail.com>
2022-12-20 11:20:31 +0800

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import AutoBuy from '../components/AutoBuy'
 
+
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <AutoBuy />
+    <div>
+      <Head><title>AutoBot</title></Head>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <AutoBuy />
+      </div>
     </div>
   )
 }
